82d0234c3dd41595a6205b898442649334757f17
hena1086 <henoktilahun1086@gmail.com>
2023-03-26 11:31:23 +0300

    adding geomap

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,8 @@ import Back from "../common/back/Back"
 import "./contact.css"
 
 const Contact = () => {
-  const map = 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d904726.6131739549!2d85.24565535!3d27.65273865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2snp!4v1652535615693!5m2!1sen!2snp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" '
+  const map = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2746.312437463985!2d38.749896202261084!3d9.030478478315088!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b85f137eb0829%3A0xc367647315386be6!2sEliana%20Hotel%20%7C%20Piazza!5e0!3m2!1sen!2snl!4v1679819252088!5m2!1sen!2snl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade'
+
   return (
     <>
       <Back title='Contact us' />
@@ -38,7 +39,7 @@ const Contact = () => {
                 <input type='email' placeholder='Email' />
               </div>
               <input type='text' placeholder='Subject' />
-              <textarea cols='30' rows='10' placeholder="jhjhjhj">
+              <textarea cols='30' rows='10' placeholder="write something here...">
                 
               </textarea>
               <button className='primary-btn'>SEND MESSAGE</button>
